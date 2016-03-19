cb0f8129586664996d84e2a0406c1c884671e126
ilkkar <mustikka8@gmail.com>
2016-03-19 17:42:21 +0200

    db

--- server.js
+++ server.js
@@ -96,7 +96,8 @@ app.post("/register", function(req, res){
 });
 
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connect('ds013579.mlab.com:13579/paikka');
+//var db = mongoose.connect('mongodb://localhost/test');
 var UserSchema=new mongoose.Schema({
 	firstname: String,
 	lastname: String,
